21f969ac400a71e21257354fa7e378ec2420a33e
Abel Molnar - mac <abel.capable@hotmail.com>
2018-02-09 22:48:04 +0100

    save into plist, codable items

--- Todoey/Controller/TodoListViewController.swift
+++ Todoey/Controller/TodoListViewController.swift
@@ -10,18 +10,17 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["Find mike", "Buy eggs", "Destroy Death Star"]
-    let defaults = UserDefaults.standard
+    var itemArray:[Item] = []
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loadItems()
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
     }
 
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -29,7 +28,11 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemList", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+     
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? UITableViewCellAccessoryType.checkmark : UITableViewCellAccessoryType.none
         
         return cell
     }
@@ -37,11 +40,9 @@ class TodoListViewController: UITableViewController {
     //MARK - Tableview delefate methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-             tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -55,10 +56,17 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add new Todoey Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //uialert button clicked
-            self.itemArray.append(textField.text ?? "New Item")
-            self.defaults.set(self.itemArray, forKey: "TodoList")
-            self.tableView.reloadData()
-        
+            let item = Item()
+            if let text = textField.text {
+                item.title = text
+            } else {
+                item.title = "New item"
+            }
+            item.done = false
+            
+            self.itemArray.append(item)
+            self.saveItems()
+            
         }
         
         alert.addTextField { (alertTextField) in
@@ -73,7 +81,33 @@ class TodoListViewController: UITableViewController {
         
     }
     
+    //MARK: model manupilation methods
+    func saveItems() {
+        
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(self.itemArray)
+            try data.write(to: self.dataFilePath!)
+            
+        } catch {
+            print("error encoding data to filepath!")
+        }
+        
+        self.tableView.reloadData()
     
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding data!")
+            }
+        }
+    }
     
 }
 
---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Abel Molnar on 2018. 01. 08..
+//  Copyright © 2018. Abel Molnar. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
